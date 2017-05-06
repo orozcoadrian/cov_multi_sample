a846d3d4d5c7940385f49db5505b9feed6865eff
orozcoadrian <orozcoadrian@gmail.com>
2017-05-05 20:14:07 -0700

    codecov configured. now adding coverage

---
+++ api/src/test/java/org/gradle/api/PersonListTest.java
@@ -0,0 +1,18 @@
+package org.gradle.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ajo on 5/5/17.
+ */
+public class PersonListTest {
+    @Test
+    public void doSomethingWithImpl() throws Exception {
+        PersonList instance = new PersonList();
+        instance.doSomethingWithImpl();
+        // TODO: need asserts
+    }
+
+}
